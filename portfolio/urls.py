b25fc52a17b7ca7640a4e37779f32308c2b47fe2
azarcb <azar.j@cittabase.com>
2022-06-01 16:00:13 +0530

    wokring django

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from xml.etree.ElementInclude import include
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
