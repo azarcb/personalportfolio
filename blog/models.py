d5b07d8080fef422d4b28936ee2fe3d5f3c9b662
azarcb <azar.j@cittabase.com>
2022-06-01 17:44:59 +0530

    blogs model is added to db

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,12 @@
+from dataclasses import field
 from django.db import models
+
 
 # Create your models here.
 
 
 class Blogs(models.Model):
-    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    pub_date = models.DateField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='imgaes/')
