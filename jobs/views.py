b39ed6c6054a8f434da52976a8aaa1014832887a
azarcb <azar.j@cittabase.com>
2022-06-01 22:52:36 +0530

    Image Added

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,9 @@
 from urllib import request
 from django.shortcuts import render
 
+from .models import Job
+
 # Create your views here.
 def home(request):
-    return render(request,'jobs/home.html')
+    jobs = Job.objects
+    return render(request,'jobs/home.html',{'jobs':jobs})
