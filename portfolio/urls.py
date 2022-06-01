211b1b9d30054325b904c206dd55435709962193
azarcb <azar.j@cittabase.com>
2022-06-01 21:22:59 +0530

    pull req

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,7 +19,9 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
+import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',jobs.views.home),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
